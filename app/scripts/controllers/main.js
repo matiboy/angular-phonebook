6d1da4fbbd833c7d8b743b8ceaedba00c7b803e1
matiboy <mathieu.chauvinc@gmail.com>
2014-11-18 14:14:42 +0800

    Clear form, add default type and remove entries

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,12 +10,19 @@
 angular.module('angularPhonebookApp')
   .controller('MainCtrl', function ($scope) {
     $scope.entries = [];
+    $scope.type = 'house';
     $scope.addEntry = function() {
       var newEntry = {
         name: $scope.name,
         type: $scope.type,
         details: $scope.details
       };
+      $scope.name = '';
+      $scope.details = '';
       $scope.entries.push(newEntry);
+    };
+
+    $scope.removeEntry = function(index) {
+      $scope.entries.splice(index, 1);
     };
   });
